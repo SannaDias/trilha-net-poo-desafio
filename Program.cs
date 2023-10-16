584eb7945c54af755b8af9afda60f7418bd639c5
SannaDias <sannapdias@gmail.com>
2023-10-15 21:18:20 -0300

    todo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone iphone = new Iphone(numero: "859923456", modelo: "15", imei: 24356, memoria: 125);
+iphone.Ligar();
+iphone.InstalarAplicativo("Call of dutty mobile");
+
+Smartphone nokia = new Nokia(numero: "85993445866", modelo: "55", imei: 87534, memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
